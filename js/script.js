61ddb3c07a28996ccfab8a14e9dddac2cddb4f47
Rodrigodgx <rdg.gasparini@gmail.com>
2025-06-13 22:58:22 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,4 +0,0 @@
-// Adicione aqui seu código JavaScript para funcionalidades interativas.
-// Por exemplo, inicialização de galerias de imagens, animações, etc.
-
-console.log("Arquivo script.js carregado!");
